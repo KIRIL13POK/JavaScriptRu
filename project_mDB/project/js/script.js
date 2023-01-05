053a845dc0a35ddfd3291f6d5605e07a311a5a68
Kiril Pokrovskiy <p1kr1vskiy@gmail.com>
2023-01-05 16:07:53 +0100

    Ejercio para avanzar con Document Object Model (DOM)

--- project_mDB/project/js/script.js
+++ project_mDB/project/js/script.js
@@ -24,3 +24,39 @@ const movieDB = {
     ]
 };
 
+//***************************************  1  ****************************************
+const adv = document.querySelectorAll('.promo__adv img');// dentro de este class sus imagines.
+//console.log(adv);
+adv.forEach(item => {
+    item.remove();
+});
+
+//****************************************  2  *****************************************
+const poster = document.querySelector('.promo__bg');
+//Ahora el genero lo sacamos directamente de poster:
+const genre = poster.querySelector('.promo__genre');
+//Para cambiar texto metodo .textContent():
+genre.textContent = 'драма';
+
+//****************************************  3  *****************************************
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+//****************************************  4/5  ****************************************
+//Primer paso eliminar los elimentos que ya tiene la pagina:
+const movieList = document.querySelector('.promo__interactive-list');
+//Para limpiat este elemento el metodo .innerHTML:
+movieList.innerHTML = "";
+movieDB.movies.sort();
+movieDB.movies.forEach(function (film, i) {
+    movieList.innerHTML += ` 
+    <li class="promo__interactive-item">${i + 1}-
+    ${film}
+    <div class="delete"></div>
+    </li>
+    `; 
+
+
+});
+
+
+
